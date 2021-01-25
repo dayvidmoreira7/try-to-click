2f9819b59976b39c8011059f0c42c66b8f2f0d91
Dayvid Montovanelli Moreira <dmoreira@gyramais.com>
2021-01-25 11:28:02 -0300

    changes
    - add rotation on target
    - improve style

---
+++ src/js/index.js
@@ -0,0 +1,27 @@
+const getElement = (selector) => {
+  return document.querySelector(selector);
+};
+
+const getRandomValue = (max) => {
+  return Math.floor(Math.random() * max);
+};
+
+const trueOrFalse = (percentage) => {
+  return getRandomValue(100) <= percentage;
+};
+
+getElement('#interative-word').style.left = `${getRandomValue(80)}%`;
+getElement('#interative-word').style.top = `${getRandomValue(80)}%`;
+
+getElement('#interative-word').addEventListener('mouseover', ({ target: { style }, target }) => {
+  style.left = `${getRandomValue(80)}%`;
+  style.top = `${getRandomValue(80)}%`;
+  style.transform = trueOrFalse(40) ? `rotateZ(${trueOrFalse(50) ? '' : '-'}${getRandomValue(90) * 4}deg)` : 'rotateZ(0deg)'
+  
+  if (tries > 10) target.innerHTML = '<span>Ta dificil? Tenta mais</span>'
+  if (tries > 20) target.innerHTML = '<span>Melhor desistir hein</span>'
+});
+
+getElement('#interative-word').addEventListener('click', ({ target }) => {
+  alert('Parabéns! Conseguiu clicar!');
+});
